1ede9d17e1ccab8d48e4e69e53a19db5913db479
anahibetancort <anahi.betancort@gmail.com>
2023-05-24 21:13:40 -0300

    mercadoLiebre

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const path = require('path');
 app.use(express.static('public'))
 
 
-app.listen(3001, () => {
-    console.log('servidor corriendo puesto 3001')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
 })
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
